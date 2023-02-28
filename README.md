0cf737efc4e5a08d81c99c93e44d04c1fe25ba31
zefanyang <446128357@qq.com>
2023-02-28 22:17:34 +0800

    update readme

--- README.md
+++ README.md
@@ -6,12 +6,18 @@ This repo contains source code for the paper: **Non-iterative Scribble-Supervise
 
 | Dataset                                                      | Image Modality          | Target Antomy                               | Scribble                                                     | #Images | Median Spacing          | Center-Crop Size |
 | ------------------------------------------------------------ | ----------------------- | ------------------------------------------- | ------------------------------------------------------------ | ------- | ----------------------- | ---------------- |
-| **CHAOS** [[challenge website]](https://chaos.grand-challenge.org/) | MRI T1-DUAL and T2-SPIR | Liver, Left-kidney, Right-kidney, Spleen    | Manual [[download link]](https://drive.google.com/file/d/1LFfso17fxPaCcwcQJ4lzyKKG22EKGnlt/view?usp=share_link) | 1917    | 1.62$\times$1.62 mm$^2$ | 256$\times$256   |
-| **ACDC** [[challenge website]](https://www.creatis.insa-lyon.fr/Challenge/acdc/databases.html) | Cine-MRI                | Right ventricle, Myocardium, Left ventricle | Manual [[download link]](https://vios-s.github.io/multiscale-adversarial-attention-gates/data) | 1902    | 1.51$\times$1.51 mm$^2$ | 224$\times$224   |
+| **CHAOS** [[challenge website]](https://chaos.grand-challenge.org/) | MRI T1-DUAL and T2-SPIR | Liver, Left-kidney, Right-kidney, Spleen    | Manual [[download link]](https://drive.google.com/file/d/1LFfso17fxPaCcwcQJ4lzyKKG22EKGnlt/view?usp=share_link) | 1,917   | 1.62$\times$1.62 mm$^2$ | 256$\times$256   |
+| **ACDC** [[challenge website]](https://www.creatis.insa-lyon.fr/Challenge/acdc/databases.html) | Cine-MRI                | Right ventricle, Myocardium, Left ventricle | Manual [[download link]](https://vios-s.github.io/multiscale-adversarial-attention-gates/data) | 1,902   | 1.51$\times$1.51 mm$^2$ | 224$\times$224   |
 | **LVSC** [[challenge website]](https://www.cardiacatlas.org/challenges/lv-segmentation-challenge/) | MRI '2D+time'           | Myocardium                                  | Artificial                                                   | 29,086  | 1.48$\times$1.48 mm$^2$ | 256$\times$256   |
 
-### Data loader
+### Data
+
+We use five-fold validation to evaluate performance. Images along with five-fold validation text files are stored in **./data/chaos/train_test_split/data_2d** and **./data/chaos/train_test_split/five_fold_split** respectively. 
+
+The dataloaders for augmenting and loading images for network learning is stored in **./datasets/chaos/chaos_dataset.py**.
 
 ### Model
+
+We train three models for comparison. They are the baseline model, our pacingpseudo model, and the fully-supervised model. The baseline is the UNet backbone penalized by the partial cross entropy loss. Our pacingpseudo uses the siamese architecture to implement the consistency training. Details are described in the paper [[arXiv]](https://arxiv.org/pdf/2210.10956.pdf). The fullly-supervised model is the UNet trained with the cross-entropy loss and Dice loss.
 
 ### Training
---
+++ datasets/chaos/chaos_aug_configs.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time  : 12/9/2021 12:20 AM
+# @Author: yzf
+"""Data augmentation configurations for CHAOS abdomen dataset."""
+from collections import namedtuple
+from datasets.augmentations import *
+
+NUM_CLASSES = 5
+IGNORED_INDEX = 5
+INPUT_SIZE = (256, 256)
+STRENGTH = 1.
+Transforms = namedtuple('Transforms', ['base_transforms', 'strong_transforms'])
+
+# Base transformation
+base_transforms = [
+        MeanStdNorm(),
+        Scaling(
+            scale_range=(0.7, 1.4),
+            num_classes=NUM_CLASSES,
+            image_scale_order=3,
+            label_scale_order=1,
+            p=0.2,
+        ),
+        ElasticTransform(
+            sigma_range=(9., 13.),
+            alpha_range=(0., 200.),
+            img_order=3,
+            lab_order=0,
+            mode='nearest',
+            clip=True,
+            p=0.2,
+        ),
+        RandomRotation(
+            degree_range=(-30, 30),
+            image_interp_order=3,
+            image_padding_val=0,
+            label_interp_order=0,
+            label_padding_val=IGNORED_INDEX,
+            p=0.2,
+        ),
+        Mirroring(
+            axis=0,
+            p=0.5
+        ),
+        Mirroring(
+            axis=1,
+            p=0.5
+        ),
+        GaussianNoise(
+            noise_scale_range=(0, 0.1),
+            p=0.15,
+        ),
+        MeanStdNorm(),
+        RandomCrop(
+            crop_size=INPUT_SIZE,
+            image_padding_value=0,
+            label_padding_value=IGNORED_INDEX,
+            p=1.
+        ),
+    ]
+
+class TransformsColor(object):
+    def __init__(self, strength=1.):
+        self.base_transforms = base_transforms
+        self.strong_transforms = self.get_strong_transforms(strength)
+
+    @staticmethod
+    def get_strong_transforms(strength):
+        color = [
+                Brightness(
+                    scale_range=(-strength*0.8, strength*0.8),
+                    p=0.8
+                ),
+                Contrast(
+                    scale_range=(max(0., 1-strength*0.8), 1+strength*0.8),
+                    p=0.8
+                ),
+                GammaAugmentation(
+                    gamma_range=(max(0., 1-strength*0.8), 1+strength*0.8),
+                    retain_stats=True,
+                    invert_data=False,
+                    p=0.8
+                ),
+            ]
+        return color
+
+class TransformsColorBlur(object):
+    def __init__(self, strength=1.):
+        self.base_transforms = base_transforms
+        self.strong_transforms = self.get_strong_transforms(strength)
+
+    @staticmethod
+    def get_strong_transforms(strength):
+        color = [
+                Brightness(
+                    scale_range=(-strength*0.8, strength*0.8),
+                    p=0.8
+                ),
+                Contrast(
+                    scale_range=(max(0., 1-strength*0.8), 1+strength*0.8),
+                    p=0.8
+                ),
+                GammaAugmentation(
+                    gamma_range=(max(0., 1-strength*0.8), 1+strength*0.8),
+                    retain_stats=True,
+                    invert_data=False,
+                    p=0.8
+                ),]
+        blur = [GaussianBlur(kernel_scale_range=(1, 1.5), p=0.8)]
+        return color + blur
+
+class TransformsColorMixup(object):
+    def __init__(self, strength=1.):
+        self.base_transforms = base_transforms
+        self.strong_transforms = self.get_strong_transforms(strength)
+
+    @staticmethod
+    def get_strong_transforms(strength):
+        color = [
+                Brightness(
+                    scale_range=(-strength*0.8, strength*0.8),
+                    p=0.8
+                ),
+                Contrast(
+                    scale_range=(max(0., 1-strength*0.8), 1+strength*0.8),
+                    p=0.8
+                ),
+                GammaAugmentation(
+                    gamma_range=(max(0., 1-strength*0.8), 1+strength*0.8),
+                    retain_stats=True,
+                    invert_data=False,
+                    p=0.8
+                ),]
+        mixup = [Mixup(lam_range=(0.8, 1.), p=0.8)]
+        return color + mixup
+
+class TransformsColorMixup(object):
+    def __init__(self, strength=1.):
+        self.base_transforms = base_transforms
+        self.strong_transforms = self.get_strong_transforms(strength)
+
+    @staticmethod
+    def get_strong_transforms(strength):
+        color = [
+                Brightness(
+                    scale_range=(-strength*0.8, strength*0.8),
+                    p=0.8
+                ),
+                Contrast(
+                    scale_range=(max(0., 1-strength*0.8), 1+strength*0.8),
+                    p=0.8
+                ),
+                GammaAugmentation(
+                    gamma_range=(max(0., 1-strength*0.8), 1+strength*0.8),
+                    retain_stats=True,
+                    invert_data=False,
+                    p=0.8
+                ),]
+        mixup = [Mixup(lam_range=(0.8, 1.), p=0.8)]
+        return color + mixup
+
+class TransformsColorLow(object):
+    def __init__(self, strength=1.):
+        self.base_transforms = base_transforms
+        self.strong_transforms = self.get_strong_transforms(strength)
+
+    @staticmethod
+    def get_strong_transforms(strength):
+        color = [
+                Brightness(
+                    scale_range=(-strength*0.8, strength*0.8),
+                    p=0.8
+                ),
+                Contrast(
+                    scale_range=(max(0., 1-strength*0.8), 1+strength*0.8),
+                    p=0.8
+                ),
+                GammaAugmentation(
+                    gamma_range=(max(0., 1-strength*0.8), 1+strength*0.8),
+                    retain_stats=True,
+                    invert_data=False,
+                    p=0.8
+                ),]
+        low = [SimulationLowRes(downscale_range=(1.5, 2), down_order=0, up_order=3, p=0.8,)]
+        return color + low
